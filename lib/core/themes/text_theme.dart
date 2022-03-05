03eedea38bf5d0b0ec26660e185c03df428c19e2
itstor <ahmdthoriq5@gmail.com>
2022-03-05 13:46:39 +0700

    feat: add textTheme

---
+++ lib/core/themes/text_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme textTheme = TextTheme(
+  headline1: GoogleFonts.mulish(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  ),
+  headline2: GoogleFonts.mulish(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  ),
+  subtitle1: GoogleFonts.mulish(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  ),
+  subtitle2: GoogleFonts.mulish(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  ),
+  bodyText1: GoogleFonts.mulish(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  ),
+  bodyText2: GoogleFonts.mulish(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  ),
+  caption: GoogleFonts.mulish(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  ),
+);
