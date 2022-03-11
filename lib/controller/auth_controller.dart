65c3e0459ca7cfcb607a8285722c7ad2f332e2c0
itstor <ahmdthoriq5@gmail.com>
2022-03-11 20:31:12 +0700

    feat: add atuh controller

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,88 @@
+import 'package:college_match/core/values/firebase_constants.dart';
+import 'package:college_match/screens/home_page/home_page.dart';
+import 'package:college_match/screens/welcome_page/welcome_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+  late Rx<User?> firebaseUser;
+
+  late Rx<GoogleSignInAccount?> googleSignInAccount;
+
+  @override
+  void onReady() {
+    super.onReady();
+
+    firebaseUser = Rx<User?>(auth.currentUser);
+    googleSignInAccount = Rx<GoogleSignInAccount?>(googleSign.currentUser);
+
+    firebaseUser.bindStream(auth.userChanges());
+    ever(firebaseUser, _setInitialScreen);
+
+    googleSignInAccount.bindStream(googleSign.onCurrentUserChanged);
+    ever(googleSignInAccount, _setInitialScreenGoogle);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      Get.offNamed("/welcome-page");
+    } else {
+      Get.offAll(() => const MyHomePage(title: 'Hello'));
+    }
+  }
+
+  _setInitialScreenGoogle(GoogleSignInAccount? googleSignInAccount) {
+    print(googleSignInAccount);
+    if (googleSignInAccount == null) {
+      Get.offNamed("/welcome-page");
+    } else {
+      Get.offAll(() => const MyHomePage(title: 'Hello'));
+    }
+  }
+
+  void signInWithGoogle() async {
+    try {
+      GoogleSignInAccount? googleSignInAccount = await googleSign.signIn();
+
+      if (googleSignInAccount != null) {
+        GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+
+        await auth
+            .signInWithCredential(credential)
+            .catchError((onErr) => print(onErr));
+      }
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      print(e.toString());
+    }
+  }
+
+  void register(String email, password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } catch (firebaseAuthException) {}
+  }
+
+  void login(String email, password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (firebaseAuthException) {}
+  }
+
+  void signOut() async {
+    await auth.signOut();
+  }
+}
