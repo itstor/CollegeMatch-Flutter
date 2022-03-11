d35463fac3b44e9ae5b8b79327b4ffaa40b8a13f
itstor <ahmdthoriq5@gmail.com>
2022-03-11 20:31:29 +0700

    feat: add firebase constant

---
+++ lib/core/values/firebase_constants.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:college_match/controller/auth_controller.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+AuthController authController = AuthController.instance;
+final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
+FirebaseAuth auth = FirebaseAuth.instance;
+FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+GoogleSignIn googleSign = GoogleSignIn();
