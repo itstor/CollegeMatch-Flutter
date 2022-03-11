e7b4915dfd783ac2e080be79d49b52f37aed5d48
itstor <ahmdthoriq5@gmail.com>
2022-03-11 20:31:47 +0700

    feat: add routes

---
+++ lib/screens/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:college_match/screens/welcome_page/welcome_page.dart';
+import 'package:college_match/screens/welcome_page/welcome_page_binding.dart';
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static final routes = [
+    GetPage(
+        name: WelcomePage.routeName,
+        page: () => WelcomePage(),
+        binding: WelcomePageBinding())
+  ];
+}
