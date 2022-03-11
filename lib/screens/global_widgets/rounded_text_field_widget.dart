a1e67fba7408c173c1e8283d19e3c6ce23328c8d
itstor <ahmdthoriq5@gmail.com>
2022-03-11 20:30:11 +0700

    feat: add rounded text field widget

---
+++ lib/screens/global_widgets/rounded_text_field_widget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+import '../../core/values/colors.dart';
+
+class RoundedTextFieldWidget extends StatelessWidget {
+  final FormFieldValidator<String>? validator;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final String hintText;
+  final Function(String)? onChanged;
+  final Function()? onTap;
+  final Function()? onEditingComplete;
+  final Function(String)? onFieldSubmitted;
+  final Function(String?)? onSaved;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final String? initialValue;
+
+  const RoundedTextFieldWidget({
+    Key? key,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction = TextInputAction.done,
+    required this.hintText,
+    this.onChanged,
+    this.onTap,
+    this.onEditingComplete,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.initialValue,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      initialValue: initialValue,
+      style: Get.textTheme.bodyText2!.copyWith(
+        fontWeight: FontWeight.w500,
+      ),
+      decoration: InputDecoration(
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+        hintText: hintText,
+        hintStyle: Get.textTheme.bodyText2!.copyWith(
+          fontWeight: FontWeight.w600,
+          color: const Color(0xFFC8C8C8),
+        ),
+        errorStyle: Get.textTheme.caption!.copyWith(
+          fontWeight: FontWeight.w500,
+          color: AppColor.kAccentColor['red']!,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide:
+              BorderSide(color: AppColor.kPrimaryPurple[100]!, width: 1.5),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide:
+              BorderSide(color: AppColor.kAccentColor['red']!, width: 1.5),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide:
+              BorderSide(color: AppColor.kAccentColor['red']!, width: 1.5),
+        ),
+        filled: true,
+        fillColor: AppColor.kGrayColor[10]!,
+        suffixIcon: suffixIcon,
+      ),
+      cursorColor: AppColor.kPrimaryPurple[100]!,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      onSaved: onSaved,
+      onChanged: onChanged,
+      obscureText: obscureText,
+    );
+  }
+}
