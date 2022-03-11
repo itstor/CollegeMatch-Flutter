10263d5688bbc81a2876b2dcd326e51d774cf0e9
itstor <ahmdthoriq5@gmail.com>
2022-03-11 20:26:18 +0700

    feat: add AppColor pallete

---
+++ lib/core/values/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Map<int, Color> kPrimaryPurple = {
+    100: Color(0xFFC4B1F8),
+    80: Color(0xFFD2C4F6),
+    50: Color(0xFFDED6F4),
+    20: Color(0xFFF2EEFC)
+  };
+
+  static const Map<int, Color> kPrimaryPink = {
+    100: Color(0xFFFDB1D5),
+    80: Color(0xFFFFD4E9),
+    50: Color(0xFFFFEAF4),
+    20: Color(0xFFFFF5FA)
+  };
+
+  static const Map<int, Color> kTyphographyColor = {
+    100: Color(0xFF1A1D26),
+    80: Color(0xFF2A2F3D),
+    50: Color(0xFF4D5364),
+    20: Color(0xFF6E7489)
+  };
+
+  static const Map<int, Color> kGrayColor = {
+    100: Color(0xFF9A9FAE),
+    80: Color(0xFFA8ACB9),
+    50: Color(0xFFC4C7D0),
+    20: Color(0xFFEBEBEB),
+    10: Color(0xFFF4F4F4)
+  };
+
+  static const Map<String, Color> kAccentColor = {
+    "red": Color(0xFFFF3666),
+  };
+}
