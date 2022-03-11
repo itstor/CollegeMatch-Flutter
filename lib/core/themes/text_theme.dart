4055ef0567fcbc80ae8424ca7b3e37b42f069152
itstor <ahmdthoriq5@gmail.com>
2022-03-11 20:26:36 +0700

    feat: change font to poppins

--- lib/core/themes/text_theme.dart
+++ lib/core/themes/text_theme.dart
@@ -2,32 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextTheme textTheme = TextTheme(
-  headline1: GoogleFonts.mulish(
+  headline1: GoogleFonts.poppins(
     fontSize: 32,
     fontWeight: FontWeight.w700,
   ),
-  headline2: GoogleFonts.mulish(
+  headline2: GoogleFonts.poppins(
     fontSize: 24,
     fontWeight: FontWeight.w700,
   ),
-  subtitle1: GoogleFonts.mulish(
+  subtitle1: GoogleFonts.poppins(
     fontSize: 18,
     fontWeight: FontWeight.w600,
   ),
-  subtitle2: GoogleFonts.mulish(
+  subtitle2: GoogleFonts.poppins(
     fontSize: 16,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
   ),
-  bodyText1: GoogleFonts.mulish(
+  bodyText1: GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.w600,
   ),
-  bodyText2: GoogleFonts.mulish(
+  bodyText2: GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.w400,
   ),
-  caption: GoogleFonts.mulish(
+  caption: GoogleFonts.poppins(
     fontSize: 12,
     fontWeight: FontWeight.w400,
   ),
+).apply(
+  displayColor: Colors.black,
+  bodyColor: Colors.black,
 );
